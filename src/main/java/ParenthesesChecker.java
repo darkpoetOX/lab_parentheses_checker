22cc84c9a0a972668c0b226ad5ee608171ba04b1
darkpoetOX <ayindejam2009@yahoo.com>
2023-07-20 14:42:35 +0100

    first passed tets EmptyString

---
+++ src/main/java/ParenthesesChecker.java
@@ -0,0 +1,11 @@
+public class ParenthesesChecker {
+
+    public boolean checkParentheses(String testString) {
+        if (testString == "") {
+            return true;
+        }
+        return false;
+    }
+}
+
+
